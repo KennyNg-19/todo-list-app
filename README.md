1293ff0490d441c1490dfd250e563b5f3fece4ea
YuhaoWU <wuyuhao2019@126.com>
2023-03-10 00:57:29 +0800

    edit upgrade npm

--- README.md
+++ README.md
@@ -1,28 +1,47 @@
+<!--
+ * @Author: YuhaoWU
+ * @Date: 2020-02-07 16:36:40
+ * @LastEditors: YuhaoWU
+ * @LastEditTime: 2023-02-08 01:05:09
+ * @Description:
+-->
+
 # todo-list-app
+
 <WebDev Bootcamp 2019> mini-project
 
 [demo](https://mini-todo-list2019.herokuapp.com/)
 
-
 ## NPM and NodeJS
-## node.js
-node.js 是一个JavaScript 运行时，**它让JavaScript 脱离了浏览器环境，可以直接在计算机上运行**，如访问PC其他组件/在server端(其他的电脑上)运行，所以让JS（或者说框架 Express) **可以作为后端语言，极大地拓展了JavaScript用途**。
 
-node.js优点：有非阻塞，事件驱动I/O等特性，从而让高并发（high concurrency）在的轮询（Polling）和comet构建的应用中成为可能
+## node.js
+
+node.js 是一个 JavaScript 运行时，**它让 JavaScript 脱离了浏览器环境，可以直接在计算机上运行**，如访问 PC 其他组件/在 server 端(其他的电脑上)运行，所以让 JS（或者说框架 Express) **可以作为后端语言，极大地拓展了 JavaScript 用途**。
+
+node.js 优点：有非阻塞，事件驱动 I/O 等特性，从而让高并发（high concurrency）在的轮询（Polling）和 comet 构建的应用中成为可能
 
 npm
-npm 是 NodeJS 的包管理器(NodeJS package manager)，一般是随同 NodeJS 一起安装，能解决 NodeJS代码部署上的很多问题：
+npm 是 NodeJS 的包管理器(NodeJS package manager)，一般是随同 NodeJS 一起安装，能解决 NodeJS 代码部署上的很多问题：
+
 - 在 Node.js 上开发时，会用到很多别人已经写好的 javascript 代码，如果每当我们需要别人的代码时，都根据名字搜索一下，下载源码，解压，再使用，会非常麻烦——于是就出现了包管理器 npm。大家把自己写好的源码上传到 npm 官网上，如果要用某个或某些个，直接通过 npm 安装就可以了
 
+## Route: 不只是指 path 变量
+
+route 路由，在 web 开发中，“route”是**指根据 url， 分配到对应的处理程序**。 就是一个**路径的解析，根据客户端提交的路径，将请求解析到相应的控制器上**。
+a mechanism where **HTTP requests are routed to the code that handles them**. To put simply, in the Router you **determine what should happen when a user visits a certain page**.
 
 ## Core knowledge points: RESTful 7 routes
+
 ![RESTful 7 routes](https://i.loli.net/2019/08/30/CLEgtMA1FNbKexj.png)
 
 ### What are routes?
+
 Routes are the code that are responsible for listening and receiving requests and then deciding what to send back.
 
 ### What is CRUD?
+
 4 basic types of (DB) actions: Create, Read, Update, and Delete.
 
 ### How this helps?
+
 By following this pattern, you don’t have to reinvent the wheel every time you build a new CRUD app. The routes and method names have already been decided so that you can run the action by directly editing URL !!!
--- app.js
+++ app.js
@@ -2,171 +2,170 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
-const _ = require('lodash')
+const _ = require("lodash");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
-mongoose.connect('mongodb+srv://admin-kenny:1@cluster0-7phca.mongodb.net/todolistDB', {
-  useNewUrlParser: true
-});
-
-
+mongoose.connect(
+  "mongodb+srv://admin-kenny:1@cluster0-7phca.mongodb.net/todolistDB",
+  {
+    useNewUrlParser: true,
+  }
+);
 
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
 // const workItems = [];
 
 const itemSchema = {
-  name: String
-}
+  name: String,
+};
 // 1. Item collection---for the HOME page's items
-const Item = mongoose.model("Item", itemSchema)
+const Item = mongoose.model("Item", itemSchema);
 // replace the items array without DBs
 const item1 = new Item({
-  name: "Welcome to the todo List"
-})
+  name: "Welcome to the todo List",
+});
 const item2 = new Item({
-  name: "Hit the + button to add a new item"
-})
+  name: "Hit the + button to add a new item",
+});
 const item3 = new Item({
-  name: "<-- Hit this to delete"
-})
-const defaultItems = [item1, item2, item3]
+  name: "<-- Hit this to delete",
+});
+const defaultItems = [item1, item2, item3];
 
 // advanced list's schema: embedded items
 const listSchema = {
   name: String,
-  items: [itemSchema]
-}
+  items: [itemSchema],
+};
 // 2. List collection---for the custom pages(except the home)
 // One doc in List , One custom page
-const List = mongoose.model("List", listSchema)
+const List = mongoose.model("List", listSchema);
 
-
-
-
-
-app.get("/", function(req, res) {
-
+app.get("/", function (req, res) {
   const day = date.getDate();
 
   // get item <-- DB
-  Item.find({}, function(err, foundItems) {
+  Item.find({}, function (err, foundItems) {
     // empty collection: 需initialize
     if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err) {
+      Item.insertMany(defaultItems, function (err) {
         if (err) console.log(err);
-        else console.log("Successfully init")
-      })
+        else console.log("Successfully init");
+      });
 
       // Insert 完毕，别忘了再次转到"/"，第二次check才能render 显示成功！！
-      res.redirect("/")
+      res.redirect("/");
     } else {
       res.render("list", {
         listTitle: day,
-        newListItems: foundItems
+        newListItems: foundItems,
       });
     }
 
     // mongoose.connection.close()
-  })
-
+  });
 });
 
 // 取代/work，通用custom型
 // app.get("/work", function(req,res){
 //   res.render("list", {listTitle: "Work List", newListItems: workItems});
 // });
-app.get("/:customListName", function(req, res) {
-  var customListName = _.capitalize(req.params.customListName)
+app.get("/:customListName", function (req, res) {
+  var customListName = _.capitalize(req.params.customListName);
   // find a List doc是否存在
-  List.findOne({
-    name: customListName
-  }, function(err, foundList) {
-    if (!err) {
-      if (!foundList) {
-        const list = new List({
-          name: customListName,
-          items: defaultItems
-        })
-        list.save()
-        console.log("new list doc: " + list.name + " inserted")
-        res.redirect('/' + customListName)
-      } else {
-        res.render("list", {
-          listTitle: customListName,
-          newListItems: foundList.items
-        });
+  List.findOne(
+    {
+      name: customListName,
+    },
+    function (err, foundList) {
+      if (!err) {
+        if (!foundList) {
+          const list = new List({
+            name: customListName,
+            items: defaultItems,
+          });
+          list.save();
+          console.log("new list doc: " + list.name + " inserted");
+          res.redirect("/" + customListName);
+        } else {
+          res.render("list", {
+            listTitle: customListName,
+            newListItems: foundList.items,
+          });
+        }
       }
     }
-  })
-})
+  );
+});
 
 /* One form, ONE post!!*/
 // for 增, from the add form
-app.post("/", function(req, res) {
-
+app.post("/", function (req, res) {
   const itemName = req.body.newItem;
-  const listName = req.body.list // html tag's value: route indentifier
+  const listName = req.body.list; // html tag's value: route indentifier
 
   // new item created MUST!
   const item = new Item({
     name: itemName,
-  })
+  });
 
-  if(listName == "Today"){ // current page
-    item.save()
-    res.redirect("/")
-  }else{
+  if (listName == "Today") {
+    // current page
+    item.save();
+    res.redirect("/");
+  } else {
     // find the list
-    List.findOne({name: listName}, function(err, foundList){
+    List.findOne({ name: listName }, function (err, foundList) {
       // it is the foundList's item!!
-      foundList.items.push(item)
+      foundList.items.push(item);
       // don't forget save for permanent storage
-      foundList.save()
-      res.redirect('/'+listName)
-    })
+      foundList.save();
+      res.redirect("/" + listName);
+    });
   }
-
 });
 
 // for 删, delete的post--- checkbox's form
-app.post("/delete", function(req, res) {
-  const checkedItemId = req.body.checkbox
-  const listName = req.body.listName
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
 
-  if(listName === "Today"){
+  if (listName === "Today") {
     // Item: will search all DB by its ID
-    Item.findByIdAndRemove(checkedItemId, function(err) {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
       if (err) console.log(err);
-      else console.log("delete Successfully")
-    })
-    res.redirect("/")
-  }else{
+      else console.log("delete Successfully");
+    });
+    res.redirect("/");
+  } else {
     // cannot done only with mongoose operator!! needs mongodb $ operators
     // 或者用PUT + method override
-    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}},
-                          function(err, foundList){
-      if(!err) res.redirect('/'+listName)
-    })
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } },
+      function (err, foundList) {
+        if (!err) res.redirect("/" + listName);
+      }
+    );
   }
-
-})
-
-
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
